42e05d65f5014d7ef0dd946d34d212d57f3f8825
zacanger <zac@zacanger.com>
2020-03-21 17:40:57 -0600

    chore: deps

--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs'
 import { resolve } from 'path'
 import isFile from 'zeelib/lib/is-file'
 
-const uniq = <T>(a: T[]): T[] => [...new Set(a)]
+const uniq = <T>(a: Array<T>): Array<T> => [...new Set(a)]
 
 export class PasswordValidationError extends Error {
   constructor(message: string) {
@@ -15,41 +15,35 @@ export class PasswordValidationError extends Error {
 type Method = 'md5' | 'sha1' | 'sha256' | 'sha512'
 
 const hashString = (method: Method) => (s = ''): string =>
-  createHash(method)
-    .update(s)
-    .digest('hex')
+  createHash(method).update(s).digest('hex')
 
 const md5String = hashString('md5')
 const sha1String = hashString('sha1')
 const sha256String = hashString('sha256')
 const sha512String = hashString('sha512')
 
-const hashWord = (s: string = ''): string[] => [
+const hashWord = (s: string = ''): Array<string> => [
   md5String(s),
   sha1String(s),
   sha256String(s),
   sha512String(s),
 ]
 
-const buildWordList = (dict: string): string[] =>
+const buildWordList = (dict: string): Array<string> =>
   readFileSync(dict, 'utf8')
     .split('\n')
     .filter((a) => a)
 
-const reverseString = (s: string = ''): string =>
-  s
-    .split('')
-    .reverse()
-    .join('')
+const reverseString = (s: string = ''): string => s.split('').reverse().join('')
 
-const buildDefaultWordList = (): string[] =>
+const buildDefaultWordList = (): Array<string> =>
   buildWordList(resolve(__dirname, 'cracklib-small.txt'))
 
 const trim = (s: string = ''): string => s.trim()
 
 const normalize = (s: string = ''): string => trim(s.toLowerCase())
 
-export const getWords = (dict: string, loose?: boolean): string[] => {
+export const getWords = (dict: string, loose?: boolean): Array<string> => {
   const words = isFile(dict) ? buildWordList(dict) : buildDefaultWordList()
   const forward = (words.length ? words : buildDefaultWordList()).map(
     loose ? trim : normalize
@@ -68,7 +62,7 @@ export const getWords = (dict: string, loose?: boolean): string[] => {
 }
 
 // like a binary search
-export const includes = (el: string, xs: string[]): boolean => {
+export const includes = (el: string, xs: Array<string>): boolean => {
   // i have no idea how efficient native includes actually is.
   // probably a linear search though?
   if (xs.length < 500) {
